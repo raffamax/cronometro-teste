6b957bd10ed74878c51b63bdd3a2b29ba92e99e3
Rafa Max <60128672+raffamax@users.noreply.github.com>
2022-09-29 18:22:49 -0300

    Add files via upload

---
+++ cronometro/scripts.js
@@ -0,0 +1,47 @@
+var ms = 0
+var sec = 0
+var min = 0
+var hour = 0
+var interval;
+
+function twodigits(digit) {
+    if(digit < 10){
+        return ('0'+ digit)
+    } else return (digit)
+}
+
+function start() {
+    interval = setInterval(cronos, 10)
+}
+
+function pause() {
+    clearInterval(interval)
+}
+
+function stop() {
+    clearInterval(interval)
+    ms = 0
+    sec = 0
+    min = 0
+    hour = 0
+    document.getElementById('time').innerText = '00:00:00:00'
+
+}
+
+function cronos() {
+    ms++
+    if(ms==100){
+        sec++
+        ms = 0
+    } if (sec==60){
+        min++
+        sec = 0
+        ms = 0
+    } if (min==60){
+        hour++
+        min = 0
+        sec = 0
+        ms = 0
+    }
+    document.getElementById('time').innerText = twodigits(hour)+':'+twodigits(min)+':'+twodigits(sec)+':'+twodigits(ms)
+}
